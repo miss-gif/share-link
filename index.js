0007dc96b4b5ad456240836ccbffd88d9f47ddd0
pc <svx327@naver.com>
2025-01-23 20:46:01 +0900

    Add : 코드 개선

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-console.log("Hello World");
-
-// Get the modal
+// 모달 요소 가져오기
 var openModalBtn = document.getElementById("openModalBtn");
 const modal = document.getElementById("myModal");
 const closeBtn = document.getElementById("closeBtn");
 
+// 모달 열기 버튼 클릭 이벤트 리스너
 openModalBtn.addEventListener("click", function () {
   console.log("Modal clicked");
   modal.style.display = "block";
 });
 
+// 모달 닫기 버튼 클릭 이벤트 리스너
 closeBtn.addEventListener("click", function () {
   console.log("Close clicked");
   modal.style.display = "none";
 });
 
+// 모달 외부 클릭 시 모달 닫기
 window.addEventListener("click", function (event) {
   if (event.target == modal) {
     console.log("Outside modal clicked");
@@ -22,19 +23,21 @@ window.addEventListener("click", function (event) {
   }
 });
 
+// URL 복사 버튼 클릭 이벤트 리스너
 const copyUrlBtn = document.getElementById("copyUrlBtn");
 copyUrlBtn.addEventListener("click", function () {
   const url = window.location.href;
   navigator.clipboard
     .writeText(url)
     .then(function () {
-      alert("URL copied to clipboard");
+      alert("URL이 복사되었습니다.");
     })
     .catch(function (error) {
       console.error("Error copying URL: ", error);
     });
 });
 
+// 카카오톡 공유 버튼 생성
 Kakao.Share.createDefaultButton({
   container: "#kakaotalk-sharing-btn",
   objectType: "feed",
